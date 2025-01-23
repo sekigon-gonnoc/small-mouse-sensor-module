1b1a678d53d8245e60495064e6867a00bf637c1e
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2025-01-23 13:10:02 +0000

    Update 2025-01-23 13:10:02

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
+# 14mmマウスセンサーモジュール
 
+14x14mmサイズのマウスセンサーモジュールです。
+
+超省電力ワイヤレスマウスセンサーであるPAW3222を使用しています。動作電流はPMW3610の半分以下となっています。（PMW3610: 0.60mA, 本モジュール: 0.27mA, データシートより）
+
+3線SPIでの動作のほか、CSをGNDに固定して2線SPIで通信することもできます。
+MOTION出力も備えています。
+
+[BOOTHにて販売中](https://nogikes.booth.pm/items/6520217)
+
+## ピン配置
+
+0.5mmピッチ、6ピンのFPCで接続します。
+同一電極面のFPCを使う場合、対向する側のコネクタでピン順が反転するので注意してください。
+
+|ピン番号|信号|
+|-|-|
+|1|3.3V|
+|2|CS|
+|3|MOTION|
+|4|SDIO|
+|5|SCLK|
+|6|GND|
+
+![](img/image.png)
+
+## サンプルプログラム
+
+* [Arduino](sample_sketch)
+
+![](img/connection.png)
+
+### 参考
+
+* [Zephyr](https://docs.zephyrproject.org/latest/build/dts/api/bindings/input/pixart%2Cpaw32xx.html)
+* [QMK](https://github.com/qmk/qmk_firmware/blob/163b894b97a5c8cf9ab80179b4d59999077ad8d5/drivers/sensors/paw3204.c)
+
+## 寸法
+
+* ![](img/size.png)
+
+## 使用例
+
+12mmトラックボール
+
+### 部品表
+
+* マウスセンサーモジュール
+* [ケース](trackball-module-case/case-12mm-1-Body.step)（3Dプリント）
+* [12mm PPボール](https://ja.aliexpress.com/item/1005007093832604.html)
+* 1mm鋼玉x3
---
+++ sample_sketch/sample_sketch.ino
@@ -0,0 +1,136 @@
+#include "Mouse.h"
+
+// Pin definitions (xiao rp2350)
+#define CLK_PIN 27  // D1
+#define DATA_PIN 28 // D2
+#define MOT_PIN 5   // D3
+
+// Write 1 byte using 2MHz SPI, mode 2, bit-banging
+void writeSPI(uint8_t address, uint8_t data) {
+  address |= 0x80;
+
+  // Send address
+  for (int i = 7; i >= 0; i--) {
+    digitalWrite(DATA_PIN, (address >> i) & 0x01); // Send from MSB
+    digitalWrite(CLK_PIN, LOW);                    // Clock Low
+    delayMicroseconds(0.25);     // 1MHz clock (T=1µs → half period 0.5µs)
+    digitalWrite(CLK_PIN, HIGH); // Clock High
+    delayMicroseconds(0.25);
+  }
+
+  // Send data
+  for (int i = 7; i >= 0; i--) {
+    digitalWrite(DATA_PIN, (data >> i) & 0x01); // Send from MSB
+    digitalWrite(CLK_PIN, LOW);                 // Clock Low
+    delayMicroseconds(0.25);
+    digitalWrite(CLK_PIN, HIGH); // Clock High
+    delayMicroseconds(0.25);
+  }
+
+  digitalWrite(DATA_PIN, HIGH);
+}
+
+// Read 1 byte using 2MHz SPI, mode 2, bit-banging
+uint8_t readSPI(uint8_t address) {
+  address &= 0x7F;
+  uint8_t data = 0;
+
+  // Send address
+  for (int i = 7; i >= 0; i--) {
+    digitalWrite(DATA_PIN, (address >> i) & 0x01); // Send from MSB
+    digitalWrite(CLK_PIN, LOW);                    // Clock Low
+    delayMicroseconds(0.25);
+    digitalWrite(CLK_PIN, HIGH); // Clock High
+    delayMicroseconds(0.25);
+  }
+
+  // Receive data
+  pinMode(DATA_PIN, INPUT); // Switch data pin to input mode
+  delayMicroseconds(1);
+
+  for (int i = 7; i >= 0; i--) {
+    digitalWrite(CLK_PIN, LOW); // Clock Low
+    delayMicroseconds(0.25);
+    digitalWrite(CLK_PIN, HIGH);          // Clock High
+    data |= (digitalRead(DATA_PIN) << i); // Receive from MSB
+    delayMicroseconds(0.25);
+  }
+  pinMode(DATA_PIN, OUTPUT); // Switch data pin back to output mode
+  digitalWrite(DATA_PIN, HIGH);
+
+  return data;
+}
+
+void setup() {
+  // Set pin modes
+  pinMode(CLK_PIN, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(MOT_PIN, INPUT);
+
+  digitalWrite(CLK_PIN, HIGH); // Initial clock state (High when idle)
+
+  // initialize mouse control:
+  Mouse.begin();
+
+  uint8_t pid0 = readSPI(0x00);
+  uint8_t pid1 = readSPI(0x01);
+
+  // Assert reset
+  writeSPI(0x06, 0xD1);
+
+  delay(35);
+
+  // Enable 12bit mode
+  //  writeSPI(0x09, 0x5A, CS_PIN);
+  //  writeSPI(0x19, 0x04, CS_PIN);
+  //  writeSPI(0x09, 0x00, CS_PIN);
+
+  // Wakeup from sleep
+  writeSPI(0x06, 0x11);
+
+  delay(35);
+
+  // Deassert reset
+  writeSPI(0x05, 0xB9);
+
+  delay(35);
+
+  // Read some register
+  readSPI(0x02);
+  readSPI(0x03);
+  readSPI(0x04);
+  readSPI(0x12);
+
+  // Reset bus state
+  digitalWrite(CLK_PIN, LOW);
+  delay(1);
+  digitalWrite(CLK_PIN, HIGH);
+  delay(2);
+}
+
+void loop() {
+  static unsigned long last_send_time;
+  static unsigned long last_read_time;
+
+  // if (digitalRead(MOT_PIN) == 0) {
+  unsigned long current_time = millis();
+  int8_t buf[4];
+  buf[0] = readSPI(0x02);
+  buf[1] = readSPI(0x03);
+  buf[2] = readSPI(0x04);
+
+  // // Read upper 4bit of XY data
+  // buf[3] = readSPI(0x12, -1);
+
+  // Check motion flag
+  if (buf[0] & 0x80) {
+    Serial.printf("%4d, %4d, %d\n", buf[1], buf[2],
+                  current_time - last_read_time);
+    last_send_time = current_time;
+
+    Mouse.move(buf[1], buf[2], 0);
+  }
+
+  last_read_time = current_time;
+  // }
+}
